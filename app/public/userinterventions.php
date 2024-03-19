f82402e32d5f4a102e840886d48ddc233d873c9d
ClementChastagner <96829145+ClementChastagner@users.noreply.github.com>
2024-03-19 18:38:16 +0100

    Update userinterventions.php

--- app/public/userinterventions.php
+++ app/public/userinterventions.php
@@ -1,15 +1,15 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+session_start(); // Start the session
+
+require_once '../vendor/autoload.php';
 use App\Page;
 
 $page = new Page();
 
-$interventions = $page->getAllInterventions(); 
-
+$interventions = $page->getUserInterventions(); 
 
 echo $page->render('userinterventions.html.twig', ['interventions' => $interventions]);
-
 
 
 ?>
