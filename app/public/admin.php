42304a1fa15b431f73fd889b455c3647a668c40d
ClementChastagner <96829145+ClementChastagner@users.noreply.github.com>
2024-03-19 19:39:30 +0100

    Update admin.php

--- app/public/admin.php
+++ app/public/admin.php
@@ -1,12 +1,27 @@
 <?php
+
+// fichier fonctionnant avec update_user.php et delete_user.php
+
+
+session_start();
+
 require_once '../vendor/autoload.php';
 
 use App\Page;
 
 $page = new Page();
-$msg = '';
 
-$interventions = $page->getAllInterventions(); // Récupère toutes les interventions depuis la base de données
+// Vérification si l'utilisateur est bien un admin
+if (isset($_SESSION['statut']) && $_SESSION['statut'] !== 1) {
+    // Si différent, redirection
+    header("Location: login.php");
+    exit;
+}
 
-echo $page->render('admin.html.twig', ['interventions' => $interventions]);
+
+$users = $page->getAllUsers();
+
+echo $page->render('admin.html.twig', ['users' => $users]);
+
 ?>
+
